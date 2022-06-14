e4a4289891a85640c2c8d4daac51770b29fdaa6a
mikhail <mikhail@bashchkov.com>
2022-06-14 17:15:20 +0400

    Initial commit

---
+++ helloapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+import os
+
+
+def homepage(request):
+    service = os.environ.get('K_SERVICE', 'Unknown service')
+    revision = os.environ.get('K_REVISION', 'Unknown revision')
+    
+    return render(request, 'homepage.html', context={
+        "message": "It's running!",
+        "Service": service,
+        "Revision": revision,
+    })
+
+def aboutpage(request):
+    return render(request, 'aboutpage.html', context={})
